4b69f6f87ca9a17720908a2cb169f5179fbdbc33
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-11-10 17:23:00 +0100

    changed form submit logic

--- js/form.js
+++ js/form.js
@@ -5,24 +5,11 @@ function handleFormSubmit(e){
     const mood = e.target.mood.value;
     const pizza = pizzaObjectCreator(type, taste, mood);
     appendPizzaToLocalStorage(pizza);
-    document.getElementById('form_dialog_text_id').innerText = "New pizza added. There are " + getPizzaListFromLocalStorage().length + " pizzas now"; 
-    $("#form_dialog_id").dialog('open');
-    e.target.reset();
+    e.target.submit();
 
 }
 
 document.addEventListener('DOMContentLoaded', function(){
     const pizzaForm = document.getElementById('pizza_form_id');
-    const dialog = $("#form_dialog_id").dialog({
-        resizable: false,
-        draggable: false,
-        autoOpen: false,
-        buttons: {
-            'OK': function() {
-                dialog.dialog('close');
-            }
-        }
-    });
-
     pizzaForm.onsubmit = handleFormSubmit;
 })
